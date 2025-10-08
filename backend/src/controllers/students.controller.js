cc76be4cb88a06fc20eeda2c125f3f8e659b45b7
Francisco Bourquin <fran.bourquin89@gmail.com>
2025-10-08 13:38:10 -0300

    Se crea la función createStudent en controllers y services

--- backend/src/controllers/students.controller.js
+++ backend/src/controllers/students.controller.js
@@ -1,9 +1,30 @@
-export const getStudentsController = async ();
+import { createStudentService } from "../services/students.service.js";
 
-export const getStudentByIdController = async ();
+export const getStudentsController = async () => {
 
-export const createStudentController = async ();
+};
 
-export const updateStudentByIdController = async ();
+export const getStudentByIdController = async () => {
 
-export const deleteStudentByIdController = async ();
+};
+
+export const createStudentController = async (req, res) => {
+    const studentInfo = req.body;
+
+    try {
+       const student = await createStudentService(studentInfo);
+       res.status(201).json({message: "Estudiante creado con éxito!", student});
+        }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
+
+}
+
+export const updateStudentByIdController = async () => {
+
+};
+
+export const deleteStudentByIdController = async () => {
+    
+};
--- backend/src/services/students.service.js
+++ backend/src/services/students.service.js
@@ -1,12 +1,27 @@
-import mongoose from "mongoose";
 import { studentModel } from "../models/students.model.js";
 
-export const getStudentsService = async ();
+export const getStudentsService = async () => {
 
-export const getStudentByIdService = async ();
+};
 
-export const createStudentService = async ();
+export const getStudentByIdService = async () => {
 
-export const updateStudentByIdService = async ();
+};
 
-export const deleteStudentByIdService = async ();
+export const createStudentService = async (studentInfo) => {
+    try {
+        const student = await studentModel.create(studentInfo);
+        return student;
+    } catch (error) {
+        throw new Error(error.message);
+        
+    }
+}
+
+export const updateStudentByIdService = async () => {
+
+};
+
+export const deleteStudentByIdService = async () => {
+    
+};
