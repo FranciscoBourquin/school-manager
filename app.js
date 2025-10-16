6e101cc893dfc6b4dc9dd260bae1ee5490232bfc
Francisco Bourquin <fran.bourquin89@gmail.com>
2025-10-16 17:54:28 -0300

    reestructuración del proyecto

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import { studentsRouter } from "./routes/students.routes.js";
+
+const PORT = process.env.PORT; 
+
+const app = express();
+
+app.use(express.json());
+app.get("/", ()=> console.log("API en Vercel funcionando"));
+app.use("/api/students/", studentsRouter)
+
+connectDB()
+.then(console.log(`Servidor escuchando en el puerto: ${PORT}`));
+
+export default app;
---
+++ controllers/students.controller.js
@@ -0,0 +1,116 @@
+import { createStudentService, deleteStudentByIdService, getStudentByIdService, getStudentsByCourseService, getStudentsService, updateStudentByIdService } from "../services/students.service.js";
+
+export const getStudentsController = async (req, res) => {
+    const { course } = req.query;
+    try {
+        if (course) {
+            const students = await getStudentsByCourseService(course);
+            if (students.length === 0) {
+                return res.status(200).json({
+                    message: `No hay estudiantes inscriptos en el curso de ${course}`,
+                    students})
+            }
+        }
+
+        const students = await getStudentsService();
+
+        if (students.length === 0) return res.status(200).json({
+            message: "No tenemos estudiantes incriptos en este momento", 
+            students
+        })
+
+        res.status(200).json({
+            message: "Estudiantes encontrados con éxito!",
+            students
+        })
+
+        
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+};
+
+export const getStudentByIdController = async (req, res) => {
+    const {id} = req.params;
+    const student = await getStudentByIdService(id)
+    if(!student) {
+        return res.status(404).json({
+            message: `Estudiante con ID ${id} no encontrado`,
+            student
+        })
+    }
+
+    res.status(200).json({
+        message: `Estudiante con ID ${id} encontrado con éxito!`,
+        student 
+    })
+
+    try {
+        
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+
+};
+
+export const createStudentController = async (req, res) => {
+    const studentInfo = req.body;
+
+    try {
+       const student = await createStudentService(studentInfo);
+       res.status(201).json({message: "Estudiante creado con éxito!", student});
+        }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
+
+}
+
+export const updateStudentByIdController = async (req, res) => {
+    const {id} = req.params;
+    const studentInfo = req.body;
+    try {
+        const updatedStudent = await updateStudentByIdService(id, studentInfo);
+
+        if (!updatedStudent) {
+            res.status(404).json({
+                message: `Alumno con ID ${id} no encontrado!`,
+                updatedStudent
+            })
+        }
+
+        res.status(200).json({
+            message: `Alumno con ID ${id} actualizado con éxito!`,
+            updatedStudent
+        })
+        
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+
+};
+
+export const deleteStudentByIdController = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deleteStudent = await deleteStudentByIdService(id);
+        if (!deleteStudent){
+            return res.status(404).json({
+                message: `Estudinte con ID ${id} no encontrado`,
+                deleteStudent
+            })
+        }
+
+        res.status(200).json({
+            message: `Estudiante con ID ${id} eliminado con éxito!`,
+            deleteStudent
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+
+};
