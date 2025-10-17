7d6d435302560fbfbc8baa9fafc800829d866d50
Francisco Bourquin <122411274+FranciscoBourquin@users.noreply.github.com>
2025-10-17 12:22:17 -0300

    Coreción de la ruta raíz

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.get("/", ()=> console.log("API en Vercel funcionando"));
+app.get("/", (req, res)=> res.send("API en Vercel funcionando"));
 app.use("/api/students/", studentsRouter)
 
 connectDB()
