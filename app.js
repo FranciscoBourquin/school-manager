2c13801aeca0870a8cdb346619a29db8073b01c6
Francisco Bourquin <fran.bourquin89@gmail.com>
2025-10-18 14:18:38 -0300

    se agrega middleware de cors

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import { studentsRouter } from "./routes/students.routes.js";
+import cors from "cors"
+import { corsConfig } from "./config/cors.config.js";
 
 const PORT = process.env.PORT; 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsConfig));
 app.get("/", (req, res)=> res.send("API en Vercel funcionando"));
-app.use("/api/students/", studentsRouter)
+app.use("/api/students/", studentsRouter);
+
+
 
 connectDB()
 .then(console.log(`Servidor escuchando en el puerto: ${PORT}`));
